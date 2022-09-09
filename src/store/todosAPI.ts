df4b2c03b135a489129bff8f2da86056767fcbba
Saibaken <shelamov.s@gmail.com>
2022-09-09 16:59:02 +0300

    feat: :sparkles: Описал работу с API

---
+++ src/store/todosAPI.ts
@@ -0,0 +1,83 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { Todo, TodoState } from "../types/todoTypes";
+
+export const getTodos = createAsyncThunk<
+  Todo[],
+  undefined,
+  { rejectValue: string }
+>("todos/getTodos", async function (_, { rejectWithValue }) {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?userId=1"
+  );
+  if (!response.ok) return rejectWithValue("Can't get todos, server error");
+  return (await response.json()) as Todo[];
+});
+
+export const postTodo = createAsyncThunk<Todo, string, { rejectValue: string }>(
+  "todos/postTodo",
+  async (title, { rejectWithValue }) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: 1,
+        title,
+        completed: false,
+      }),
+    });
+    if (!response.ok) return rejectWithValue("Can't make todo, server error");
+    return (await response.json()) as Todo;
+  }
+);
+
+export const toggleTodo = createAsyncThunk<
+  Todo,
+  number,
+  { rejectValue: string; state: { todos: TodoState } }
+>("todos/toggleTodo", async (id, { rejectWithValue, getState }) => {
+  const todo: Todo | undefined = getState().todos.list.find(
+    (todo) => todo.id === id
+  );
+  if (todo) {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          completed: !todo.completed,
+        }),
+      }
+    );
+    if (!response.ok) return rejectWithValue("Can't update todo, server error");
+    return (await response.json()) as Todo;
+  }
+  return rejectWithValue("Can't find todo");
+});
+
+export const deleteTodo = createAsyncThunk<
+  Todo,
+  number,
+  { rejectValue: string, state: { todos: TodoState } }
+>("todos/deleteTodo", async (id, { rejectWithValue, getState }) => {
+  console.log(getState);
+  console.log(getState());
+  const todo = getState().todos.list.find((todo) => todo.id === id);
+  console.log(2);
+  if (todo) {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    console.log(response);
+    if (!response.ok) return rejectWithValue("Can't delete todo, server error");
+    return todo;
+  }
+  return rejectWithValue("Can't find todo");
+});
