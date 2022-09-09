6aedfc1b72be6f7869a8e23cccd881828fbac810
Saibaken <shelamov.s@gmail.com>
2022-09-09 17:00:26 +0300

    feat: :sparkles: Описал компонент todo

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useAppDispatch } from '../../hooks/hooks'
+import { deleteTodo, toggleTodo, postTodo } from '../../store/todosAPI';
+import { Todo } from '../../types/todoTypes'
+
+
+const TodoItem: React.FC<Todo> = (todo) => {
+  const dispatch = useAppDispatch();
+  const id = todo.id;
+  const handleButton = () => {
+    dispatch(deleteTodo(id))
+  }
+
+  const test = () => {
+    dispatch(postTodo('asd'));
+  }
+
+  const handleCheckbox = () => {
+    dispatch(toggleTodo(id));
+  }
+
+  return (
+      <label className='todo-item'>
+        <input className='todo-item__checkbox' type="checkbox" id={String(todo.id)} onChange={handleCheckbox}/>
+        <h3 className='todo-item__title' >{todo.title}</h3>
+        <button className='todo-item__delete' onClick={handleButton}>❌</button>
+      </label>
+  )
+}
+
+export default TodoItem;
