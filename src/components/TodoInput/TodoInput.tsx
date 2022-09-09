1349c38fb6d2309bc4934c37093ab18c12e18290
Saibaken <shelamov.s@gmail.com>
2022-09-09 17:01:01 +0300

    feat: :sparkles: Описал компонент todo input

---
+++ src/components/TodoInput/TodoInput.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type TodoInputProps = {
+    value: string;
+    updateValue: (str: string) => void;
+    handleSubmit: () => void;
+}
+
+const TodoInput: React.FC<TodoInputProps> = ({ value, updateValue, handleSubmit }) => {
+    return (
+        <div className='todo-input'>
+            <input className='todo-input__input-field' type="text" value={value} onChange={(e) => updateValue(e.target.value)} />
+            <button className='todo-input__submit-button' onClick={handleSubmit}>Add Todo</button>
+        </div>
+    )
+}
+
+export default TodoInput;
