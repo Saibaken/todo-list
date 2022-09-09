098faff8e14833909eac5be0a7718c86ea6dd9c9
Saibaken <shelamov.s@gmail.com>
2022-09-09 17:01:43 +0300

    feat: :sparkles: Написал app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import './App.css';
+import TodoList from './components/TodoList/TodoList';
+import { useAppDispatch, useAppSelector } from './hooks/hooks'
+import { useEffect, useState } from 'react'
+import { getTodos, postTodo } from './store/todosAPI';
+import TodoInput from './components/TodoInput/TodoInput';
 
 function App() {
+  const [input, setInput] = useState('');
+  const dispatch = useAppDispatch();
+  const { loading, error } = useAppSelector(state => state.todos);
+
+  const updateValue = (str: string) => {
+    setInput(str);
+  }
+
+  const handleSubmit = () => {
+    if (input.trim().length) {
+      dispatch(postTodo(input.trim()));
+      setInput('');
+    }
+  }
+
+  useEffect(() => {
+    dispatch(getTodos())
+  }, [dispatch])
+
   return (
     <div>
-
+      <TodoInput value={input} updateValue={updateValue} handleSubmit={handleSubmit}/>
+      {<h2>Loading</h2> && loading}
+      {<h2>Error</h2> && error}
+      <TodoList />
     </div>
   );
 }
