71cde8990d97729a3a924531609b1c95d053ba7b
Saibaken <shelamov.s@gmail.com>
2022-09-09 17:00:45 +0300

    feat: :sparkles: Описал компонент todo list

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useAppSelector } from '../../hooks/hooks'
+import TodoItem from '../TodoItem/TodoItem'
+
+export default function TodoList() {
+    const todos = useAppSelector(state => state.todos.list);
+
+  return (
+    <ul className='todo-list'>
+        {todos.map(todo => {
+            return (
+                <TodoItem key={todo.id} {...todo}/>
+            )
+        })}
+    </ul>
+  )
+}
