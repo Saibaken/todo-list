d70a5d524e640bc383056804ad0dc5f74ba2f8d1
Saibaken <shelamov.s@gmail.com>
2022-09-10 20:45:31 +0300

    feat: :lipstick: Написал стили для TodoItem

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useAppDispatch } from '../../hooks/hooks'
 import { deleteTodo, toggleTodo } from '../../store/todosAPI';
 import { Todo } from '../../types/todoTypes'
+import classNames from 'classnames';
+import './TodoItem.css'
 
 
 const TodoItem: React.FC<Todo> = (todo) => {
@@ -16,11 +18,11 @@ const TodoItem: React.FC<Todo> = (todo) => {
   }
 
   return (
-      <label className='todo-item'>
+      <li className={classNames('todo-item', {'todo-item_done': todo.completed})}>
         <input className='todo-item__checkbox' type="checkbox" id={String(todo.id)} onChange={handleCheckbox} checked={todo.completed}/>
         <h3 className='todo-item__title' >{todo.title}</h3>
         <button className='todo-item__delete' onClick={handleButton}>❌</button>
-      </label>
+      </li>
   )
 }
 
